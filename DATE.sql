6ee7e090c41422fcb7f200867afd0c7d56d7dbd5
Peeyush Upadhyay <51726991+Peeyush-Now@users.noreply.github.com>
2022-05-16 09:54:29 +1200

    Upload time of the day dimension

---
+++ DATE.sql
@@ -0,0 +1,58 @@
+use database nandos_anz_dw;
+use schema presentation;
+
+create or replace view date as
+select
+    DATE_KEY                   as "Date Key"
+    , CALENDAR_DATE            as "Calendar Date"
+    , DATE_DESC                as "Date Desc"
+    , DAY_NAME                 as "Day Name"
+    , DAY_NAME_ABRV            as "Day Name Abrv"
+    , DAY_IN_WEEK_NO           as "Day In Week No"
+    , DAY_IN_MONTH             as "Day In Month"
+    , DAY_STATUS               as "Day Status"
+    , WEEKDAY_FLAG             as "Weekday Flag"
+    , WEEKEND_FLAG             as "Weekend Flag"
+    , WEEK_COMMENCING_DATE_KEY as "Week Commencing Date Key"
+    , WEEK_COMMENCING_DATE     as "Week Commencing Date"
+    , WEEK_ENDING_DATE_KEY     as "Week Ending Date Key"
+    , WEEK_ENDING_DATE         as "Week Ending Date"
+    , CAL_YEAR                 as "Calendar Year"
+    , MONTH_NO                 as "Calendar Month No"
+    , MONTH                    as "Calendar Month"
+    , MONTH_NAME               as "Calendar Month Name"
+    , MONTH_NAME_ABRV          as "Calendar Month Name Abrv"
+    , MONTHEND_FLAG            as "Calendar Monthend Flag"
+    , CAL_WEEK_IN_YEAR         as "Calendar Week In Year"
+    , CAL_DAY_IN_YEAR          as "Calendar Day In Year"
+    , CAL_QUARTER_NO           as "Calendar Quarter No"
+    , CAL_QUARTER              as "Calendar Quarter"
+    , FIN_YEAR                 as "Financial Year"
+    , FIN_YEAR_NAME            as "Financial Year Name"
+    , FIN_YEAR_STATUS          as "Financial Year Status"
+    , FIN_YEARHALF             as "Financial Yearhalf"
+    , FIN_PERIOD               as "Financial Period"
+    , FIN_PERIOD_STATUS        as "Financial Period Status"
+    , FIN_QUARTER              as "Financial Quarter"
+    , FIN_QUARTER_NO           as "Financial Quarter No"
+    , FIN_QUARTER_STATUS       as "Financial Quarter Status"
+    , FIN_WEEK_IN_YEAR         as "Financial Week In Year"
+    , FIN_WEEK_STATUS          as "Financial Week Status"
+    , FIN_DAY_IN_YEAR          as "Financial Day In Year"
+    , CURRENT_CAL_WTD          as "Current Cal WTD"
+    , CURRENT_CAL_MTD          as "Current Cal MTD"
+    , CURRENT_CAL_PTD          as "Current Cal PTD"
+    , CURRENT_CAL_QTD          as "Current Cal QTD"
+    , CURRENT_CAL_YTD          as "Current Cal YTD"
+    , PREVIOUS_WK_CAL_WTD      as "Previous Wk Cal WTD"
+    , PREVIOUS_YR_CAL_WTD      as "Previous Yr Cal WTD"
+    , PREVIOUS_YR_CAL_PTD      as "Previous Yr Cal PTD"
+    , PREVIOUS_YR_CAL_QTD      as "Previous Yr Cal QTD"
+    , PREVIOUS_YR_CAL_YTD      as "Previous Yr Cal YTD"
+    , case 
+        when CURRENT_CAL_WTD = 'YES' then 'WTD'
+        when PREVIOUS_WK_CAL_WTD = 'YES' then 'WTD-LW'
+        WHEN PREVIOUS_YR_CAL_WTD = 'YES' then 'WTD-LY'
+        end "Financial Week WTD"
+from
+    dim_date;
---
+++ dim_time_of_day.sql
@@ -0,0 +1,131 @@
+-- Dim Time of Day
+
+CREATE OR REPLACE TABLE PRESENTATION.DIM_TIME_OF_DAY AS
+    SELECT
+        TIME_PK TIME_OF_DAY_KEY
+        , HOUR_24 TIME_OF_DAY_HH24
+        , HOUR_12 TIME_OF_DAY_HH12
+        -- , HOUR_1 HOUR_OF_DAY
+        , FLOOR(TIME_PK/60) MINUTE_OF_DAY
+        , INTERVAL_05 INTERVAL_05_MIN
+        , INTERVAL_15 INTERVAL_15_MIN
+        , INTERVAL_30 INTERVAL_30_MIN
+        , INTERVAL_1HR INTERVAL_01_HOUR
+        , SERVING_TIME
+        , LAST_UPDATE_DATE
+FROM (
+      select
+            to_date('00:00:00','HH24:MI:SS') as TEMP_TIMESTAMP,
+            seq8() as TEMP_SEQUENCE,
+            row_number() over (order by TEMP_SEQUENCE) as TIME_PK,
+            -- to_time(to_char(dateadd(second,TEMP_SEQUENCE,TEMP_TIMESTAMP),'HH24:MI:SS'),'HH24:MI:SS') HOUR_24, --original
+            to_char(dateadd(second,TEMP_SEQUENCE,TEMP_TIMESTAMP),'HH24:MI:SS') HOUR_24,
+            to_char(dateadd(second,TEMP_SEQUENCE,TEMP_TIMESTAMP),'HH12:MI:SS PM') HOUR_12,
+            to_char(dateadd(second,TEMP_SEQUENCE,TEMP_TIMESTAMP),'HH24') as HOUR_1,
+            to_char(dateadd(second,TEMP_SEQUENCE,TEMP_TIMESTAMP),'MI') as MINUTE_1,
+            to_char(dateadd(second,TEMP_SEQUENCE,TEMP_TIMESTAMP),'SS') as SECOND_1,
+            case
+                when to_time(to_char(dateadd(second,TEMP_SEQUENCE,TEMP_TIMESTAMP),'HH24:MI:SS'),'HH24:MI:SS') <
+                to_time(to_char(dateadd(second,TEMP_SEQUENCE,TEMP_TIMESTAMP),'HH24:')||'05:00','HH24:MI:SS') then
+                to_char(dateadd(second,TEMP_SEQUENCE,TEMP_TIMESTAMP),'HH24')||':00:00-'||
+                to_char(dateadd(second,TEMP_SEQUENCE,TEMP_TIMESTAMP),'HH24')||':04:59'
+                when to_time(to_char(dateadd(second,TEMP_SEQUENCE,TEMP_TIMESTAMP),'HH24:MI:SS'),'HH24:MI:SS') between
+                to_time(to_char(dateadd(second,TEMP_SEQUENCE,TEMP_TIMESTAMP),'HH24:')||'05:00','HH24:MI:SS') and
+                to_time(to_char(dateadd(second,TEMP_SEQUENCE,TEMP_TIMESTAMP),'HH24:')||'09:59','HH24:MI:SS')	then
+                to_char(dateadd(second,TEMP_SEQUENCE,TEMP_TIMESTAMP),'HH24')||':05:00-'||
+                to_char(dateadd(second,TEMP_SEQUENCE,TEMP_TIMESTAMP),'HH24')||':09:59'
+                when to_time(to_char(dateadd(second,TEMP_SEQUENCE,TEMP_TIMESTAMP),'HH24:MI:SS'),'HH24:MI:SS') between
+                to_time(to_char(dateadd(second,TEMP_SEQUENCE,TEMP_TIMESTAMP),'HH24:')||'10:00','HH24:MI:SS') and
+                to_time(to_char(dateadd(second,TEMP_SEQUENCE,TEMP_TIMESTAMP),'HH24:')||'14:59','HH24:MI:SS')	then
+                to_char(dateadd(second,TEMP_SEQUENCE,TEMP_TIMESTAMP),'HH24')||':10:00-'||
+                to_char(dateadd(second,TEMP_SEQUENCE,TEMP_TIMESTAMP),'HH24')||':14:59'
+                when to_time(to_char(dateadd(second,TEMP_SEQUENCE,TEMP_TIMESTAMP),'HH24:MI:SS'),'HH24:MI:SS') between
+                to_time(to_char(dateadd(second,TEMP_SEQUENCE,TEMP_TIMESTAMP),'HH24:')||'15:00','HH24:MI:SS') and
+                to_time(to_char(dateadd(second,TEMP_SEQUENCE,TEMP_TIMESTAMP),'HH24:')||'19:59','HH24:MI:SS')	then
+                to_char(dateadd(second,TEMP_SEQUENCE,TEMP_TIMESTAMP),'HH24')||':15:00-'||
+                to_char(dateadd(second,TEMP_SEQUENCE,TEMP_TIMESTAMP),'HH24')||':19:59'
+                when to_time(to_char(dateadd(second,TEMP_SEQUENCE,TEMP_TIMESTAMP),'HH24:MI:SS'),'HH24:MI:SS') between
+                to_time(to_char(dateadd(second,TEMP_SEQUENCE,TEMP_TIMESTAMP),'HH24:')||'20:00','HH24:MI:SS') and
+                to_time(to_char(dateadd(second,TEMP_SEQUENCE,TEMP_TIMESTAMP),'HH24:')||'24:59','HH24:MI:SS')	then
+                to_char(dateadd(second,TEMP_SEQUENCE,TEMP_TIMESTAMP),'HH24')||':20:00-'||
+                to_char(dateadd(second,TEMP_SEQUENCE,TEMP_TIMESTAMP),'HH24')||':24:59'
+                when to_time(to_char(dateadd(second,TEMP_SEQUENCE,TEMP_TIMESTAMP),'HH24:MI:SS'),'HH24:MI:SS') between
+                to_time(to_char(dateadd(second,TEMP_SEQUENCE,TEMP_TIMESTAMP),'HH24:')||'25:00','HH24:MI:SS') and
+                to_time(to_char(dateadd(second,TEMP_SEQUENCE,TEMP_TIMESTAMP),'HH24:')||'29:59','HH24:MI:SS')	then
+                to_char(dateadd(second,TEMP_SEQUENCE,TEMP_TIMESTAMP),'HH24')||':25:00-'||
+                to_char(dateadd(second,TEMP_SEQUENCE,TEMP_TIMESTAMP),'HH24')||':29:59'
+                when to_time(to_char(dateadd(second,TEMP_SEQUENCE,TEMP_TIMESTAMP),'HH24:MI:SS'),'HH24:MI:SS') between
+                to_time(to_char(dateadd(second,TEMP_SEQUENCE,TEMP_TIMESTAMP),'HH24:')||'30:00','HH24:MI:SS') and
+                to_time(to_char(dateadd(second,TEMP_SEQUENCE,TEMP_TIMESTAMP),'HH24:')||'34:59','HH24:MI:SS')	then
+                to_char(dateadd(second,TEMP_SEQUENCE,TEMP_TIMESTAMP),'HH24')||':30:00-'||
+                to_char(dateadd(second,TEMP_SEQUENCE,TEMP_TIMESTAMP),'HH24')||':34:59'
+                when to_time(to_char(dateadd(second,TEMP_SEQUENCE,TEMP_TIMESTAMP),'HH24:MI:SS'),'HH24:MI:SS') between
+                to_time(to_char(dateadd(second,TEMP_SEQUENCE,TEMP_TIMESTAMP),'HH24:')||'35:00','HH24:MI:SS') and
+                to_time(to_char(dateadd(second,TEMP_SEQUENCE,TEMP_TIMESTAMP),'HH24:')||'39:59','HH24:MI:SS')	then
+                to_char(dateadd(second,TEMP_SEQUENCE,TEMP_TIMESTAMP),'HH24')||':35:00-'||
+                to_char(dateadd(second,TEMP_SEQUENCE,TEMP_TIMESTAMP),'HH24')||':39:59'
+                when to_time(to_char(dateadd(second,TEMP_SEQUENCE,TEMP_TIMESTAMP),'HH24:MI:SS'),'HH24:MI:SS') between
+                to_time(to_char(dateadd(second,TEMP_SEQUENCE,TEMP_TIMESTAMP),'HH24:')||'40:00','HH24:MI:SS') and
+                to_time(to_char(dateadd(second,TEMP_SEQUENCE,TEMP_TIMESTAMP),'HH24:')||'44:59','HH24:MI:SS')	then
+                to_char(dateadd(second,TEMP_SEQUENCE,TEMP_TIMESTAMP),'HH24')||':40:00-'||
+                to_char(dateadd(second,TEMP_SEQUENCE,TEMP_TIMESTAMP),'HH24')||':44:59'
+                when to_time(to_char(dateadd(second,TEMP_SEQUENCE,TEMP_TIMESTAMP),'HH24:MI:SS'),'HH24:MI:SS') between
+                to_time(to_char(dateadd(second,TEMP_SEQUENCE,TEMP_TIMESTAMP),'HH24:')||'45:00','HH24:MI:SS') and
+                to_time(to_char(dateadd(second,TEMP_SEQUENCE,TEMP_TIMESTAMP),'HH24:')||'49:59','HH24:MI:SS')	then
+                to_char(dateadd(second,TEMP_SEQUENCE,TEMP_TIMESTAMP),'HH24')||':45:00-'||
+                to_char(dateadd(second,TEMP_SEQUENCE,TEMP_TIMESTAMP),'HH24')||':49:59'
+                when to_time(to_char(dateadd(second,TEMP_SEQUENCE,TEMP_TIMESTAMP),'HH24:MI:SS'),'HH24:MI:SS') between
+                to_time(to_char(dateadd(second,TEMP_SEQUENCE,TEMP_TIMESTAMP),'HH24:')||'50:00','HH24:MI:SS') and
+                to_time(to_char(dateadd(second,TEMP_SEQUENCE,TEMP_TIMESTAMP),'HH24:')||'54:59','HH24:MI:SS')	then
+                to_char(dateadd(second,TEMP_SEQUENCE,TEMP_TIMESTAMP),'HH24')||':50:00-'||
+                to_char(dateadd(second,TEMP_SEQUENCE,TEMP_TIMESTAMP),'HH24')||':54:59'
+                when to_time(to_char(dateadd(second,TEMP_SEQUENCE,TEMP_TIMESTAMP),'HH24:MI:SS'),'HH24:MI:SS') between
+                to_time(to_char(dateadd(second,TEMP_SEQUENCE,TEMP_TIMESTAMP),'HH24:')||'55:00','HH24:MI:SS') and
+                to_time(to_char(dateadd(second,TEMP_SEQUENCE,TEMP_TIMESTAMP),'HH24:')||'59:59','HH24:MI:SS')	then
+                to_char(dateadd(second,TEMP_SEQUENCE,TEMP_TIMESTAMP),'HH24')||':55:00-'||
+                to_char(dateadd(second,TEMP_SEQUENCE,TEMP_TIMESTAMP),'HH24')||':59:59'
+            end INTERVAL_05,
+            case
+                when to_time(to_char(dateadd(second,TEMP_SEQUENCE,TEMP_TIMESTAMP),'HH24:MI:SS'),'HH24:MI:SS') <
+                to_time(to_char(dateadd(second,TEMP_SEQUENCE,TEMP_TIMESTAMP),'HH24:')||'15:00','HH24:MI:SS') then
+                to_char(dateadd(second,TEMP_SEQUENCE,TEMP_TIMESTAMP),'HH24')||':00:00-'||
+                to_char(dateadd(second,TEMP_SEQUENCE,TEMP_TIMESTAMP),'HH24')||':14:59'
+                when to_time(to_char(dateadd(second,TEMP_SEQUENCE,TEMP_TIMESTAMP),'HH24:MI:SS'),'HH24:MI:SS') between
+                to_time(to_char(dateadd(second,TEMP_SEQUENCE,TEMP_TIMESTAMP),'HH24:')||'15:00','HH24:MI:SS') and
+                to_time(to_char(dateadd(second,TEMP_SEQUENCE,TEMP_TIMESTAMP),'HH24:')||'29:59','HH24:MI:SS') then
+                to_char(dateadd(second,TEMP_SEQUENCE,TEMP_TIMESTAMP),'HH24')||':15:00-'||
+                to_char(dateadd(second,TEMP_SEQUENCE,TEMP_TIMESTAMP),'HH24')||':29:59'
+                when to_time(to_char(dateadd(second,TEMP_SEQUENCE,TEMP_TIMESTAMP),'HH24:MI:SS'),'HH24:MI:SS') between
+                to_time(to_char(dateadd(second,TEMP_SEQUENCE,TEMP_TIMESTAMP),'HH24:')||'30:00','HH24:MI:SS') and
+                to_time(to_char(dateadd(second,TEMP_SEQUENCE,TEMP_TIMESTAMP),'HH24:')||'44:59','HH24:MI:SS') then
+                to_char(dateadd(second,TEMP_SEQUENCE,TEMP_TIMESTAMP),'HH24')||':30:00-'||
+                to_char(dateadd(second,TEMP_SEQUENCE,TEMP_TIMESTAMP),'HH24')||':44:59'
+                when to_time(to_char(dateadd(second,TEMP_SEQUENCE,TEMP_TIMESTAMP),'HH24:MI:SS'),'HH24:MI:SS') between
+                to_time(to_char(dateadd(second,TEMP_SEQUENCE,TEMP_TIMESTAMP),'HH24:')||'45:00','HH24:MI:SS') and
+                to_time(to_char(dateadd(second,TEMP_SEQUENCE,TEMP_TIMESTAMP),'HH24:')||'59:59','HH24:MI:SS') then
+                to_char(dateadd(second,TEMP_SEQUENCE,TEMP_TIMESTAMP),'HH24')||':45:00-'||
+                to_char(dateadd(second,TEMP_SEQUENCE,TEMP_TIMESTAMP),'HH24')||':59:59'
+            end INTERVAL_15,
+            case
+                when to_time(to_char(dateadd(second,TEMP_SEQUENCE,TEMP_TIMESTAMP),'HH24:MI:SS'),'HH24:MI:SS') <
+                to_time(to_char(dateadd(second,TEMP_SEQUENCE,TEMP_TIMESTAMP),'HH24:')||'30:00','HH24:MI:SS') then
+                to_char(dateadd(second,TEMP_SEQUENCE,TEMP_TIMESTAMP),'HH24')||':00:00-'||
+                to_char(dateadd(second,TEMP_SEQUENCE,TEMP_TIMESTAMP),'HH24')||':29:59'
+                when to_time(to_char(dateadd(second,TEMP_SEQUENCE,TEMP_TIMESTAMP),'HH24:MI:SS'),'HH24:MI:SS') >
+                to_time(to_char(dateadd(second,TEMP_SEQUENCE,TEMP_TIMESTAMP),'HH24:')||'29:59','HH24:MI:SS') then
+                to_char(dateadd(second,TEMP_SEQUENCE,TEMP_TIMESTAMP),'HH24')||':30:00-'||
+                to_char(dateadd(second,TEMP_SEQUENCE,TEMP_TIMESTAMP),'HH24')||':59:59'
+
+            end INTERVAL_30,
+            to_char(dateadd(second,TEMP_SEQUENCE,TEMP_TIMESTAMP),'HH24')||':00:00-'|| to_char(dateadd(second,TEMP_SEQUENCE,TEMP_TIMESTAMP),'HH24')||':59:59' as INTERVAL_1HR,
+            case
+                when to_time(to_char(dateadd(second,TEMP_SEQUENCE,TEMP_TIMESTAMP),'HH24:MI:SS'),'HH24:MI:SS') between to_time('09:00:00','HH24:MI:SS') and to_time('10:59:59','HH24:MI:SS') then 'BREAKFAST'
+                when to_time(to_char(dateadd(second,TEMP_SEQUENCE,TEMP_TIMESTAMP),'HH24:MI:SS'),'HH24:MI:SS') between to_time('11:00:00','HH24:MI:SS') and to_time('14:59:59','HH24:MI:SS')  then 'LUNCH'
+                when to_time(to_char(dateadd(second,TEMP_SEQUENCE,TEMP_TIMESTAMP),'HH24:MI:SS'),'HH24:MI:SS') between to_time('15:00:00','HH24:MI:SS') and to_time('16:59:59','HH24:MI:SS')  then 'AFTERNOON'
+                when to_time(to_char(dateadd(second,TEMP_SEQUENCE,TEMP_TIMESTAMP),'HH24:MI:SS'),'HH24:MI:SS') between to_time('17:00:00','HH24:MI:SS') and to_time('20:59:59','HH24:MI:SS')  then 'DINNER'
+                when to_time(to_char(dateadd(second,TEMP_SEQUENCE,TEMP_TIMESTAMP),'HH24:MI:SS'),'HH24:MI:SS') between to_time('21:00:00','HH24:MI:SS') and to_time('22:59:59','HH24:MI:SS')  then 'LATE NIGHT'
+                else 'OTHER' end SERVING_TIME,
+            CAST(CURRENT_TIMESTAMP AS TIMESTAMP) AS LAST_UPDATE_DATE
+
+        from table(generator(rowcount => 86400))
+    ) TEMP_QUERY;
